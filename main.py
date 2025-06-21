84890593f3b20bc27bfde743750f431ed1b501c0
tkyw <tkyw200211@gmail.com>
2025-06-21 21:56:22 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    print("Received data: ", data)
+    return "OK", 202
+
+if __name__ == "__main__":
+    app.run(port=5000)
