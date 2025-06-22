6c09ebe21dc819936a33254ccd4ef17665cffa7e
tkyw <tkyw200211@gmail.com>
2025-06-22 12:23:28 +0800

    Modified request handling

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ app = Flask(__name__)
 @app.route("/webhook", methods=["GET", "POST"])
 def webhook():
     # data = request.json
-    if request.method == 'GET' and 'validationToken' in request.args:
-        return request.args['validationToken'], 200, {'Content-Type': 'text/plain'}
+    if request.method == 'GET':
+        if 'validationToken' in request.args:
+            return request.args['validationToken'], 200, {'Content-Type': 'text/plain'}
+        else:
+            return"Webhook endpoint is running", 200
     # Handle real notifications (POST)
-    if request.method == 'POST':
+    elif request.method == 'POST':
         # Your event handling logic here
         return '', 202
 
